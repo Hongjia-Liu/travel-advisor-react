9a1db348df0810925bfc3557a186bcf77f463ec1
Hongjia Liu <hongjia_liu@outlook.com>
2022-02-03 10:08:51 +0000

    Update List - display list items

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -8,13 +8,25 @@ import {
 	FormControl,
 	Select,
 } from "@material-ui/core";
-
+import PlaceDetails from "./../PlaceDetails/PlaceDetails";
 import useStyles from "./styles";
 
 const List = () => {
 	const classes = useStyles();
 	const [type, settype] = useState("restaurants");
 	const [rating, setRating] = useState("restaurants");
+
+	const places = [
+		{ name: "Cool Place" },
+		{ name: "Best Beer" },
+		{ name: "Best Steak" },
+		{ name: "Cool Place" },
+		{ name: "Best Beer" },
+		{ name: "Best Steak" },
+		{ name: "Cool Place" },
+		{ name: "Best Beer" },
+		{ name: "Best Steak" },
+	];
 
 	return (
 		<div className={classes.container}>
@@ -41,6 +53,13 @@ const List = () => {
 					<MenuItem value={4.5}>Above 4.5</MenuItem>
 				</Select>
 			</FormControl>
+			<Grid container spacing={3} className={classes.list}>
+				{places?.map((place, i) => (
+					<Grid item key={i} xs={12}>
+						<PlaceDetails place={place} />
+					</Grid>
+				))}
+			</Grid>
 		</div>
 	);
 };
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,5 +1,5 @@
-const PlaceDetails = () => {
-	return <div>PlaceDetails</div>;
+const PlaceDetails = ({ place }) => {
+	return <h1>{place.name}</h1>;
 };
 
 export default PlaceDetails;
